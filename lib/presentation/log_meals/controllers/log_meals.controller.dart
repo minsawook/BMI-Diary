59854dfe57760358af066ba84fcf1681a63d664e
minsawook <dnr111222@gmail.com>
2023-11-12 01:10:59 +0900

    bmi 다이러리 뷰 init

--- lib/presentation/log_meals/controllers/log_meals.controller.dart
+++ lib/presentation/log_meals/controllers/log_meals.controller.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:bmi_calcur/presentation/bmi_calculator/controllers/bmi_calculator.controller.dart';
 import 'package:bmi_calcur/presentation/bmi_history/controllers/bmi_history.controller.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 import '../../../data/model/bmi_model.dart';
@@ -11,9 +12,18 @@ class LogMealsController extends GetxController {
 
   final bmiCalulController = Get.find<BmiCalculatorController>();
   RxList<BmiModel> dietList = <BmiModel>[].obs;
+  Rx<DateTime> dateTime = DateTime.now().obs;
+  late TextEditingController breakfastController;
+  late TextEditingController lunchController;
+  late TextEditingController dinnerController;
+
   @override
   void onInit() {
     super.onInit();
+    breakfastController = TextEditingController();
+    lunchController = TextEditingController();
+    dinnerController = TextEditingController();
+
     ever(bmiCalulController.bmiModelList, (callback) => initDietList());
   }
 
@@ -25,6 +35,9 @@ class LogMealsController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    breakfastController.dispose();
+    lunchController.dispose();
+    dinnerController.dispose();
   }
 
   void initDietList(){
--- lib/presentation/log_meals/log_meals.screen.dart
+++ lib/presentation/log_meals/log_meals.screen.dart
@@ -1,8 +1,12 @@
+import 'dart:ffi';
+
 import 'package:bmi_calcur/Util/util.dart';
+import 'package:bmi_calcur/presentation/component/textView.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../data/common/common.dart';
 import '../navigation/routes.dart';
 import 'controllers/log_meals.controller.dart';
 
@@ -11,51 +15,106 @@ class LogMealsScreen extends GetView<LogMealsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LogMealsScreen'),
-        centerTitle: true,
-      ),
-      body: GetBuilder<LogMealsController>(
-        builder: (_) {
-          return Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: (){
-                      Get.toNamed(Routes.ADD_LOG_MEALS,arguments: {
-                        'bmiModel' : controller.getDietList(_.dietList.value[index].time)
-                      });
-                    },
-                    child: Container(
+
+      body: Center(
+        child: Column(
+
+          children: [
+            Container(
+              color: Colors.red,
+              height: 260,
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.yellow,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //일
+                    Container(
                       width: double.infinity,
-                      height: 100,
-                      child: Text(_.dietList.value[index].diet[0]),
+                      height: 32,
+                      color: Common.lineColor,
+                      child: TextView(
+                        left: 16,
+                        txt: '${controller.dateTime.value.day} 일',
+                        size: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  );
-                },
-                itemCount: _.dietList.value.length,
+                    //아침
+                    dietBar(
+                        barColor: const Color(0xfff5f5f5),
+                        controller: controller.breakfastController,
+                        title: '아침'
+                    ),
+                    dietBar(
+                        barColor: const Color(0xffebebeb),
+                        controller: controller.breakfastController,
+                        title: '점심'
+                    ),
+                    dietBar(
+                        barColor: const Color(0xfff5f5f5),
+                        controller: controller.breakfastController,
+                        title: '저녁'
+                    )
+                  ],
+                ),
               ),
-          );
-        }
-      ),
+            )
+          ],
+        ),
+      )
 
-        floatingActionButton : FloatingActionButton(
-          onPressed: () {
-            Get.toNamed(Routes.ADD_LOG_MEALS,arguments: {
-              'bmiModel' : controller.getDietList(Util().getDate(DateTime.now
-                ()))
-            });
-          },
-          backgroundColor: Colors.blue,
+        // floatingActionButton : FloatingActionButton(
+        //   onPressed: () {
+        //     Get.toNamed(Routes.ADD_LOG_MEALS,arguments: {
+        //       'bmiModel' : controller.getDietList(Util().getDate(DateTime.now
+        //         ()))
+        //     });
+        //   },
+        //   backgroundColor: Colors.blue,
+        //
+        //   shape: RoundedRectangleBorder(
+        //       borderRadius: BorderRadius.circular(45)
+        //   ),
+        //   child: const Icon(Icons.add,color: Colors.white,size: 45,weight: 23,),
+        //
+        // )
+    );
+  }
 
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(45)
+  Widget dietBar({required var title, required Color barColor, required
+  TextEditingController controller
+  }){
+    return Expanded(
+      child: Container(
+        color: barColor,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextView(
+                top: 10,
+                bottom: 10,
+                txt: title,
+                size: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              TextField(
+                controller:  controller,
+                decoration:  InputDecoration(
+                    suffix: Image.asset('${Common.imagePath}icon_edit.png'),
+
+                    border: InputBorder.none
+                ),
+              )
+            ],
           ),
-          child: const Icon(Icons.add,color: Colors.white,size: 45,weight: 23,),
-
-        )
+        ),
+      ),
     );
   }
 }
